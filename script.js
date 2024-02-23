4700b93b808415e50671291e06a0647db47be2a6
Daud Dhiya <daud.dhiya31@gmail.com>
2024-02-23 11:14:12 +0700

    feat: video #11 & #12 done

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 const close = document.querySelectorAll(".close");
 close.forEach((e) => {
   e.addEventListener("click", function (e) {
+    /* ==== DOM TRAVERSAL ==== */
     e.target.parentElement.style.display = "none";
+
+    /* ==== PREVENT DEFAULT ==== */
+    e.preventDefault();
+
+    /* ==== HANDLING EVENT BUBBLING ==== */
+    e.stopPropagation();
   });
+});
+
+const cards = document.querySelectorAll(".card");
+cards.forEach((card) => {
+  card.addEventListener("click", function () {
+    alert("ok");
+  });
+});
+
+// 2nd Method
+const container = document.querySelector(".container");
+container.addEventListener("click", function (e) {
+  if (e.target.className == "close") {
+    e.target.parentElement.style.display = "none";
+    e.preventDefault();
+  }
 });
